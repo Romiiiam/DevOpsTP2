5696012658633ce3c7eca8000f7457d66e8fbdfe
Roman <Rleyrat@sponsorise.me>
2019-02-17 22:16:59 +0100

    Ajout de fichier
    Si le lien ne fonctionne pas, aller ici :
    http://localhost/var/www/html/index.php

--- php-apache/index.php
+++ php-apache/index.php
@@ -10,20 +10,13 @@
             $username = "user";
             $password = "pwd";
             $dbname = "testdb";
-
-
             $conn = new mysqli($servername, $username, $password, $dbname);
-            
             $conn->query("CREATE TABLE testdb.'compteur' ( id INT NOT NULL AUTO_INCREMENT , value INT NOT NULL , PRIMARY KEY (id));");
-
             $conn->query("INSERT INTO testdb.'compteur' (id,'value') VALUES (1,0);");
-
             $conn->query("UPDATE compteur SET value=value+1 WHERE id=1");
-
             $res = $conn->query("SELECT value FROM compteur WHERE id=1");
             $row = $res->fetch_array(MYSQLI_NUM);
             echo "Ajout +1 et refresh : ". $row[0];
-
         ?>
     </body>
 </html>
